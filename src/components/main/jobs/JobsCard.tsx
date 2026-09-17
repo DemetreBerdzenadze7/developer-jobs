01a8f206c928f606ac71414ec288faa4e5e72572
Demetre Berdzenadze <berdzena.demetre@gmail.com>
2026-09-17 16:37:03 +0400

    feat: improve job listing display in JobsCard by adding job details and handling empty state

--- src/components/main/jobs/JobsCard.tsx
+++ src/components/main/jobs/JobsCard.tsx
@@ -45,30 +45,36 @@ const JobsCard = () => {
           </>
         ) : (
           <>
-            {filtered.map((item) => (
-              <Link key={item.id} to={`job/${item.id}`}>
-                <div className="w-87.5 h-60 bg-white rounded-main py-9 px-8 relative">
-                  <div
-                    className="w-12.5 h-12.5 rounded-[15px] flex items-center justify-center absolute -top-7 "
-                    style={{ backgroundColor: item.logoBackground }}
-                  >
-                    <img src={item.logo} alt={item.company} />
+            {filtered.length > 0 ? (
+              filtered.map((item) => (
+                <Link key={item.id} to={`job/${item.id}`}>
+                  <div className="w-87.5 h-60 bg-white rounded-main py-9 px-8 relative">
+                    <div
+                      className="w-12.5 h-12.5 rounded-[15px] flex items-center justify-center absolute -top-7"
+                      style={{ backgroundColor: item.logoBackground }}
+                    >
+                      <img src={item.logo} alt={item.company} />
+                    </div>
+                    <div className="flex items-center gap-3">
+                      <p className="text-description">{item.postedAt}</p>
+                      <div className="rounded-[50%] bg-description w-1 h-1"></div>
+                      <p className="text-description">{item.contract}</p>
+                    </div>
+                    <h2 className="text-jobs text-main font-bold my-4">
+                      {item.position}
+                    </h2>
+                    <p className="text-description">{item.company}</p>
+                    <p className="mt-5 text-country text-btn-primary font-bold">
+                      {item.location}
+                    </p>
                   </div>
-                  <div className="flex items-center gap-3">
-                    <p className="text-description">{item.postedAt}</p>
-                    <div className="rounded-[50%] bg-description w-1 h-1 "></div>
-                    <p className="text-description ">{item.contract}</p>
-                  </div>
-                  <h2 className="text-jobs text-main font-bold my-4 ">
-                    {item.position}
-                  </h2>
-                  <p className="text-description">{item.company}</p>
-                  <p className="mt-5 text-country text-btn-primary font-bold ">
-                    {item.location}
-                  </p>
-                </div>
-              </Link>
-            ))}
+                </Link>
+              ))
+            ) : (
+              <div className="col-span-full  text-2xl font-bold text-gray-400">
+                No jobs found 😕
+              </div>
+            )}
           </>
         )}
       </div>
